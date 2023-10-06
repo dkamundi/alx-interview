13f6698129b4eedc1d64f8b9401da05d39f4e635
dkamundi <davekamundi@gmail.com>
2023-10-06 03:02:45 +0000

    0. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""determines if all the boxes can be opened"""
+
 
 def canUnlockAll(boxes):
-    # Initialize a list to keep track of the status of each box (0 for closed, 1 for opened)
-    box_status = [0] * len(boxes)
+    """
+    determines if all the boxes can be opened
+    Args::
+        boxes(list): list of integer lists
+    """
+    if (len(boxes) == 0):
+        return False
 
-    # Mark the first box as opened
-    box_status[0] = 1
-
-    # Create a stack for DFS and start with the first box
-    stack = [0]
-
-    while stack:
-        current_box = stack.pop()
-
-        # Check all the keys in the current box
-        for key in boxes[current_box]:
-            # If the key opens a new box and that box is closed, mark it as opened and add it to the stack
-            if 0 <= key < len(boxes) and box_status[key] == 0:
-                box_status[key] = 1
-                stack.append(key)
-
-    # Check if all boxes are opened (i.e., all elements in box_status are 1)
-    return all(box_status)
+    for key in range(1, len(boxes) - 1):
+        checked_boxes = False
+        for idx in range(len(boxes)):
+            checked_boxes = key in boxes[idx] and key != idx
+            if checked_boxes:
+                break
+        if checked_boxes is False:
+            return False
+    return True
