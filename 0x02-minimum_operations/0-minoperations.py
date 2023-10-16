3665b73f609cd82bdb3cffbd6bb1c13c80a45484
dkamundi <davekamundi@gmail.com>
2023-10-16 02:10:49 +0000

    0. Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,18 +5,17 @@ Code
 
 
 def minOperations(n):
-    if n<= 1:
-        return n
-
-    # Initialize a list to keep track of the minimum operations for each index.
-    dp = [0] * (n + 1)
-
-    for i in range(2, n + 1):
-        dp[i] = i
-
-        for j in range(2, int(i**0.5) + 1):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + 1 // j)
-                dp[i] = min(dp[i], dp[i // j] + j)
-
-    return dp[n]
+    """
+    returns the minimum no of operations needed
+    """
+    num = 0
+    i = 2
+    if type(n) != int or n <= 1:
+        return 0
+    while n != 1:
+        if n % i == 0:
+            n = n / i
+            num = num + i
+        else:
+            i = i + 1
+    return num
