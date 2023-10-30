dbd178a8001defa6d89924ff7dda9c81ff0fef18
dkamundi <davekamundi@gmail.com>
2023-10-30 02:57:31 +0000

    solution

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,34 +1,26 @@
 #!/usr/bin/python3
+""" A method that determines if a given
+data set represents a valid UTF-8 encoding.
+"""
 
 
 def validUTF8(data):
-    # Initialize a variable to keep track of the number of expected following bytes
-    following_bytes = 0
+    """
+    method that determines if a given
+    data set represents a valid UTF-8 encoding.
+    """
+    for element in data:
+        UTF8_encoding = '08b'
+        char = format(element, UTF8_encoding)
 
-
-    # Iterate through the data list
-    for byte in data:
-        # Check if the current byte is a continuation byte
-        if following_bytes > 0:
-            # If it's not in the form "10xxxxxx," return False
-            if (byte & 0b11000000) != 0b10000000:
-                return False
-            following_bytes -= 1
+        if char.startswith('0'):
+            continue
+        elif char.startswith('110'):
+            continue
+        elif char.startswith("1110"):
+            continue
+        elif char.startswith("11110"):
+            continue
         else:
-            # Determine the number of following bytes based on the leading bits
-            if (byte & 0b10000000) == 0:
-                following_bytes = 0
-            elif (byte & 0b11100000) == 0b11000000:
-                following_bytes = 1
-            elif (byte & 0b11110000) == 0b11100000:
-                following_bytes = 2
-            elif (byte & 0b11111000) == 0b11110000:
-                following_byte = 3
-            else:
-                # If it doesn't start with valid UTF-8 leading bits, return False
-                return False
-    # If there are remaining expected following bytes, return False
-    if following_bytes > 0:
-        return False
-
+            return False
     return True
